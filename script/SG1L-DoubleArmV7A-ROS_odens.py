8e025986d5f5c0d0db6ca206e0ffc73d4cca6df3
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2021-07-30 12:05:11 +0900

    最初のコミット

---
+++ script/SG1L-DoubleArmV7A-ROS_odens.py
@@ -0,0 +1,4 @@
+import WRSUtil_odens
+WRSUtil_odens.loadProject(
+    "SingleSceneView", "SG1L", "AGXSimulator", "DoubleArmV7A_odens",
+    enableVisionSimulation = True, remoteType = "ROS_odens")
---
+++ script/SG1M-DoubleArmV7S-ROS_odens.py
@@ -0,0 +1,4 @@
+import WRSUtil_odens
+WRSUtil_odens.loadProject(
+    "SingleSceneView", "SG1M", "AISTSimulator", "DoubleArmV7S_odens",
+    enableVisionSimulation = True, remoteType = "ROS_odens")
