ae9043a64027a2252e2c04d1440c6ca4bfc5fe14
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2021-08-15 18:25:30 +0900

    スクリプト追加

---
+++ script/QR-DoubleArmV7A-ROS_odens.py
@@ -0,0 +1,4 @@
+import WRSUtil_odens
+WRSUtil_odens.loadProject(
+    "SingleSceneView", "QR", "AGXSimulator", "DoubleArmV7A_odens",
+    enableVisionSimulation = True, remoteType = "ROS_odens", taskType = "ODENS")
