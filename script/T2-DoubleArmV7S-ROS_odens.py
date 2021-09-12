369ccdb5864b56f9c726b93ae9d3985988a21cb5
HT16A086 <ht16a086@oecu.jp>
2021-09-12 21:08:17 +0900

    T2に名前を変更

---
+++ script/T2-DoubleArmV7S-ROS_odens.py
@@ -0,0 +1,4 @@
+import WRSUtil_odens
+WRSUtil_odens.loadProject(
+    "SingleSceneView", "T2", "AISTSimulator", "DoubleArmV7S_odens",
+    enableVisionSimulation = True, remoteType = "ROS_odens", taskType = "WRS2018")
