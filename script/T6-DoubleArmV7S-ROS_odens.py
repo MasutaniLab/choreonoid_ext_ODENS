085c4327cc58b10508d5554363d8d63a0fa5ed96
odens-oecu <odens.oecu@email.com>
2021-09-13 14:18:10 +0900

    ステージ6まで追加

---
+++ script/T6-DoubleArmV7S-ROS_odens.py
@@ -0,0 +1,4 @@
+import WRSUtil_odens
+WRSUtil_odens.loadProject(
+    "SingleSceneView", "T6", "AISTSimulator", "DoubleArmV7S_odens",
+    enableVisionSimulation = True, remoteType = "ROS_odens", taskType = "WRS2018")
